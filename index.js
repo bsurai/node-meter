6d82f8038bb5fb34c843bd85d438973389dbaf05
Bogdan Surai <bogdansurai@Bogdans-MacBook-Pro.local>
2018-11-20 14:13:32 +0200

    improve spawning

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const {exec} = require('child_process');
+const {spawn} = require('child_process');
 
-const cpback = exec('npm run server:dev --prefix ./backend', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-});
+const cpback = spawn('npm', ['run', 'server:dev', '-prefix', './backend']);
 cpback.stdout.pipe(process.stdout);
 cpback.stderr.pipe(process.stderr);
-
-
-const cpfront = exec('npm run start --prefix ./frontend', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
+cpback.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
 });
+cpback.on('error', (error) => {
+  console.error(`spawn error: ${error}`);
+});
+
+const cpfront = spawn('npm', ['run', 'start', '-prefix', './frontend']);
 cpfront.stdout.pipe(process.stdout);
 cpfront.stderr.pipe(process.stderr);
+cpfront.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+cpfront.on('error', (error) => {
+  console.error(`spawn error: ${error}`);
+});
